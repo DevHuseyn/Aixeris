14aa7b445d9989cfe1d1ab725031f5b530e93e2f
DevHuseyn <huseyn.dev@gmail.com>
2025-03-18 16:29:29 +0400

    Fix: Add Tailwind CSS configuration files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'tailwindcss': require.resolve('tailwindcss'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
